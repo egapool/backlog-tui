a37485be663b5b0a4bf36c4484263efa2d52e5b1
Yusuke Egami <egpersent@gmail.com>
2022-09-19 14:33:01 +0900

    Appをmoduleに切り出し

---
+++ src/app.rs
@@ -0,0 +1,94 @@
+use serde::{Deserialize, Serialize};
+use std::env;
+use tui::widgets::ListState;
+
+pub struct StatefulList<T> {
+    pub state: ListState,
+    pub items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Issue {
+    pub id: u32,
+    pub issue_key: String,
+    pub summary: String,
+    pub description: String,
+}
+
+pub struct App {
+    pub items: StatefulList<Issue>,
+}
+
+impl App {
+    pub fn with_items(items: Vec<Issue>) -> App {
+        App {
+            items: StatefulList::with_items(items),
+        }
+    }
+}
+
+pub async fn fetch_issues() -> Result<Vec<Issue>, Box<dyn std::error::Error>> {
+    let backlog_space_id = env::var("BACKLOG_SPACE_ID")?;
+    let backlog_api_key = env::var("BACKLOG_API_KEY")?;
+
+    let url = format!("https://{}.backlog.com/api/v2/issues", backlog_space_id);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .query(&[
+            ("projectId[]", "156939"),
+            ("apiKey", &backlog_api_key),
+            ("count", "100"),
+        ])
+        .send()
+        .await?;
+
+    // TODO response.status()の中身でエラーハンドリング
+
+    let issues = response.json::<Vec<Issue>>().await?;
+
+    Ok(issues)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+mod app;
+
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
-use std::env;
 
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
@@ -14,87 +14,18 @@ use tui::{
     layout::{Alignment, Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
-    widgets::{Block, Borders, List, ListItem, ListState, Paragraph, Wrap},
+    widgets::{Block, Borders, List, ListItem, Paragraph, Wrap},
     Frame, Terminal,
 };
 
-struct StatefulList<T> {
-    state: ListState,
-    items: Vec<T>,
-}
-
-impl<T> StatefulList<T> {
-    fn with_items(items: Vec<T>) -> StatefulList<T> {
-        StatefulList {
-            state: ListState::default(),
-            items,
-        }
-    }
-
-    fn next(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i >= self.items.len() - 1 {
-                    0
-                } else {
-                    i + 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
-    }
-
-    fn previous(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i == 0 {
-                    self.items.len() - 1
-                } else {
-                    i - 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
-    }
-
-    fn unselect(&mut self) {
-        self.state.select(None);
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Issue {
-    /// 課題ID
-    id: u32,
-    /// 課題のキー
-    issue_key: String,
-    /// タイトル
-    summary: String,
-    /// 概要
-    description: String,
-}
-
-struct App {
-    items: StatefulList<Issue>,
-}
-
-impl App {
-    fn with_items(items: Vec<Issue>) -> App {
-        App {
-            items: StatefulList::with_items(items),
-        }
-    }
-}
+use crate::app::App;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     // Fetch Contents
-    let issues = get_issues().await?;
+    let issues = app::fetch_issues().await?;
 
     // setup terminal
     enable_raw_mode()?;
@@ -124,7 +55,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
     loop {
-        terminal.draw(|f| ui(f, &mut app))?;
+        terminal.draw(|f| ui::draw(f, &mut app))?;
 
         if let Event::Key(key) = event::read()? {
             match key.code {
@@ -139,7 +70,6 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
 }
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
-    // Create two chunks with equal horizontal screen space
     let chunks = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
@@ -153,7 +83,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         .map(|issue| {
             let summary = vec![Spans::from(vec![
                 Span::from(issue.issue_key.clone()),
-                Span::from(" "),
+                Span::from(": "),
                 Span::from(issue.summary.clone()),
             ])];
             ListItem::new(summary).style(Style::default())
@@ -191,26 +121,3 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     f.render_widget(paragraph, chunks[1]);
 }
 
-async fn get_issues() -> Result<Vec<Issue>, Box<dyn std::error::Error>> {
-    let backlog_space_id = env::var("BACKLOG_SPACE_ID")?;
-    let backlog_api_key = env::var("BACKLOG_API_KEY")?;
-
-    let url = format!("https://{}.backlog.com/api/v2/issues", backlog_space_id);
-
-    let client = reqwest::Client::new();
-    let response = client
-        .get(url)
-        .query(&[
-            ("projectId[]", "156939"),
-            ("apiKey", &backlog_api_key),
-            ("count", "100"),
-        ])
-        .send()
-        .await?;
-
-    // TODO response.status()の中身でエラーハンドリング
-
-    let issues = response.json::<Vec<Issue>>().await?;
-
-    Ok(issues)
-}
